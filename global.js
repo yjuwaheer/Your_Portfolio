49143d99c0668aa7cd32c78d58e314b05892607c
Yudish Juwaheer <60716609+yjuwaheer@users.noreply.github.com>
2023-03-05 12:42:54 -0330

    Updated menu for smaller screens

--- global.js
+++ global.js
@@ -147,6 +147,147 @@ class Navbar extends HTMLElement {
   }
 }
 
+// Configure menu for smaller screens
+const configureMenuBurger = () => {
+  let smallMenu = `
+    <div class="w-screen h-screen bg-gray-100 fixed top-0 left-0 flex flex-col justify-center items-center hidden" id="small-menu">
+      <!-- Pages -->
+      <div class="flex flex-col">
+        <a
+          href="/"
+          class="group text-2xl font-medium text-gray-700 transition duration-300 mb-5"
+          >Home
+          <span
+            class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-gray-600"
+          ></span>
+        </a>
+        <a
+          href="/projects.html"
+          class="group text-2xl font-medium text-gray-700 transition duration-300 mb-5"
+          >Projects
+          <span
+            class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-gray-600"
+          ></span>
+        </a>
+        ${
+          data.info?.resume !== ""
+            ? `
+              <a
+                href=${data.info?.resume}
+                target="_blank"
+                class="group text-2xl font-medium text-gray-700 transition duration-300 mb-5"
+              >Resume
+                <i class="fa-regular fa-file"></i>
+                <span
+                  class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-gray-600"
+                  ></span>
+              </a>
+            `
+            : ""
+        }
+      <div>
+
+      <!-- Divider -->
+      <div class="h-[1px] bg-gray-500 my-5"></div>
+
+      <!-- Socials -->
+      <div>
+        <!-- Email -->
+        ${
+          data.connect?.email !== ""
+            ? `
+              <a href="mailto:${data.connect?.email}" target="_blank" class="mr-5">
+                <i class="fa-regular fa-envelope"></i>
+              </a>
+            `
+            : ""
+        }
+
+        <!-- LinkedIn -->
+        ${
+          data.connect?.linkedin !== ""
+            ? `
+              <a href="${data.connect?.linkedin}" target="_blank" class="mr-5">
+                <i class="fa-brands fa-linkedin-in"></i>
+              </a>
+            `
+            : ""
+        }
+
+        <!-- YouTube -->
+        ${
+          data.connect?.youtube !== ""
+            ? `
+              <a href="${data.connect?.youtube}" target="_blank" class="mr-5">
+                <i class="fa-brands fa-youtube"></i>
+              </a>
+            `
+            : ""
+        }
+
+        <!-- Facebook -->
+        ${
+          data.connect?.facebook !== ""
+            ? `
+              <a href="${data.connect?.facebook}" target="_blank" class="mr-5">
+                <i class="fa-brands fa-facebook-f"></i>
+              </a>
+            `
+            : ""
+        }
+
+        <!-- GitHub -->
+        ${
+          data.connect?.github !== ""
+            ? `
+              <a href="${data.connect?.github}" target="_blank" class="mr-5">
+                <i class="fa-brands fa-github"></i>
+              </a>
+            `
+            : ""
+        }
+
+        <!-- Instagram -->
+        ${
+          data.connect?.instagram !== ""
+            ? `
+              <a href="${data.connect?.instagram}" target="_blank" class="mr-5">
+                <i class="fa-brands fa-instagram"></i>
+              </a>
+            `
+            : ""
+        }
+
+        <!-- Codepen -->
+        ${
+          data.connect?.codepen !== ""
+            ? `
+              <a href="${data.connect?.codepen}" target="_blank" class="mr-5">
+                <i class="fa-brands fa-codepen"></i>
+              </a>
+            `
+            : ""
+        }
+      </div>
+
+      <!-- Close Button -->
+      <div class="fixed top-10 right-10 text-3xl" id="menu-close">
+        <i class="fa-solid fa-xmark"></i>
+      </div>
+    </div>
+  `;
+
+  document.body.innerHTML += smallMenu;
+
+  document.getElementById("menu-burger").addEventListener("click", () => {
+    document.getElementById("small-menu").classList.remove("hidden");
+  });
+
+  document.getElementById("menu-close").addEventListener("click", () => {
+    document.getElementById("small-menu").classList.add("hidden");
+  });
+};
+
 const initialize = async () => {
   // Get data
   await fetch("./data.json")
@@ -155,139 +296,7 @@ const initialize = async () => {
 
   customElements.define("navbar-component", Navbar);
 
-  document.getElementById("menu-burger").addEventListener("click", () => {
-    console.log("clicked");
-
-    let smallMenu = `
-      <div class="w-screen h-screen bg-gray-100 fixed top-0 left-0 flex flex-col justify-center items-center">
-        <!-- Pages -->
-        <div>
-          <a
-            href="/"
-            class="group text-lg font-medium text-gray-700 transition duration-300"
-            >Home
-            <span
-              class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-gray-600"
-            ></span>
-          </a>
-          <a
-            href="/projects.html"
-            class="group text-lg font-medium text-gray-700 transition duration-300"
-            >Projects
-            <span
-              class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-gray-600"
-            ></span>
-          </a>
-          ${
-            data.info?.resume !== ""
-              ? `
-                <a
-                  href=${data.info?.resume}
-                  target="_blank"
-                  class="group text-lg font-medium text-gray-700 transition duration-300"
-                >Resume
-                  <i class="fa-regular fa-file"></i>
-                  <span
-                    class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-gray-600"
-                    ></span>
-                </a>
-              `
-              : ""
-          }
-        <div>
-
-        <!-- Divider -->
-        <div class="h-[1px] bg-gray-500 my-5"></div>
-
-        <!-- Socials -->
-        <div>
-         <!-- Email -->
-          ${
-            data.connect?.email !== ""
-              ? `
-                <a href="mailto:${data.connect?.email}" target="_blank" class="mr-5">
-                  <i class="fa-regular fa-envelope"></i>
-                </a>
-              `
-              : ""
-          }
-
-          <!-- LinkedIn -->
-          ${
-            data.connect?.linkedin !== ""
-              ? `
-                <a href="${data.connect?.linkedin}" target="_blank" class="mr-5">
-                  <i class="fa-brands fa-linkedin-in"></i>
-                </a>
-              `
-              : ""
-          }
-
-          <!-- YouTube -->
-          ${
-            data.connect?.youtube !== ""
-              ? `
-                <a href="${data.connect?.youtube}" target="_blank" class="mr-5">
-                  <i class="fa-brands fa-youtube"></i>
-                </a>
-              `
-              : ""
-          }
-
-          <!-- Facebook -->
-          ${
-            data.connect?.facebook !== ""
-              ? `
-                <a href="${data.connect?.facebook}" target="_blank" class="mr-5">
-                  <i class="fa-brands fa-facebook-f"></i>
-                </a>
-              `
-              : ""
-          }
-
-          <!-- GitHub -->
-          ${
-            data.connect?.github !== ""
-              ? `
-                <a href="${data.connect?.github}" target="_blank" class="mr-5">
-                  <i class="fa-brands fa-github"></i>
-                </a>
-              `
-              : ""
-          }
-
-          <!-- Instagram -->
-          ${
-            data.connect?.instagram !== ""
-              ? `
-                <a href="${data.connect?.instagram}" target="_blank" class="mr-5">
-                  <i class="fa-brands fa-instagram"></i>
-                </a>
-              `
-              : ""
-          }
-
-          <!-- Codepen -->
-          ${
-            data.connect?.codepen !== ""
-              ? `
-                <a href="${data.connect?.codepen}" target="_blank" class="mr-5">
-                  <i class="fa-brands fa-codepen"></i>
-                </a>
-              `
-              : ""
-          }
-        </div>
-
-        <!-- Close Button -->
-        <div class="fixed top-10 right-10 text-3xl">
-          <i class="fa-solid fa-xmark"></i>
-        </div>
-      </div>
-    `;
-
-    document.body.innerHTML += smallMenu;
-  });
+  configureMenuBurger();
 };
 
 initialize();
